d164259669de6d7873c993ea1a9cf54d92bb432d
JoCee <joceeporter@outlook.com>
2019-04-17 18:02:20 -0600

    Looks good

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Graph from './router/graph';
+import Trades from './router/trades';
+import Team from './router/team';
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="navigation">
+        <nav >
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/team/">Team</Link>
+            </li>
+            <li>
+              <Link to="/trades/">Trades</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Graph} />
+        <Route path="/team/" component={Team} />
+        <Route path="/trades/" component={Trades} />
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
+
+
