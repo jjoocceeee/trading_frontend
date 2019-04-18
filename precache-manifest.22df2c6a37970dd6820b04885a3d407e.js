8961e9e3868de92d73262a6c302927c50757b104
JoCee <joceeporter@outlook.com>
2019-04-17 18:24:00 -0600

    Updates

---
+++ precache-manifest.22df2c6a37970dd6820b04885a3d407e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2dd22e18a48be27f79a",
+    "url": "/trading_frontend/static/js/runtime~main.7c065126.js"
+  },
+  {
+    "revision": "3cfdac87c76a2907cc31",
+    "url": "/trading_frontend/static/js/main.e0db6420.chunk.js"
+  },
+  {
+    "revision": "a0468425f71e06368d7a",
+    "url": "/trading_frontend/static/js/2.1a7b07be.chunk.js"
+  },
+  {
+    "revision": "3cfdac87c76a2907cc31",
+    "url": "/trading_frontend/static/css/main.76e330f0.chunk.css"
+  },
+  {
+    "revision": "4a250e3a8e802c2bb0ddfe75e4c12810",
+    "url": "/trading_frontend/index.html"
+  }
+];
